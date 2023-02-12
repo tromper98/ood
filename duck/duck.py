2569e7750d65ab9776f71ffaea48eb3652c6c37f
tromper98 <koloff.daniil@yandex.ru>
2023-02-12 22:15:37 +0300

    lab-01 Добавил базовый класс Duck

---
+++ duck/duck.py
@@ -0,0 +1,33 @@
+from typing import Optional
+
+from implementation.quack import QuackStrategy
+from implementation.fly import FlyStrategy
+from implementation.dance import DanceStrategy
+
+
+class Duck:
+    _quack_strategy: QuackStrategy
+    _fly_strategy: FlyStrategy
+    _dance_strategy: DanceStrategy
+
+    def __init__(self, quack_strategy: QuackStrategy, fly_strategy: FlyStrategy, dance_strategy: DanceStrategy):
+        self._quack_strategy = quack_strategy
+        self._fly_strategy = fly_strategy
+        self._dance_strategy = dance_strategy
+
+    def perform_quack(self) -> Optional[str]:
+        return self._quack_strategy.quack()
+
+    def perform_fly(self) -> Optional[str]:
+        return self._fly_strategy.fly()
+
+    def perform_dance(self) -> Optional[str]:
+        return self._dance_strategy.dance()
+
+    @staticmethod
+    def display() -> str:
+        ...
+
+    @staticmethod
+    def swim() -> str:
+        return 'плавает'
