ecc47749ae88e1699cae25dc79b02c650dddb794
tromper98 <koloff.daniil@yandex.ru>
2023-02-27 01:45:21 +0300

    lab-02 Добавил WeatherInfo и WeatherStation

--- 02_observer/interfaces/observable.py
+++ 02_observer/interfaces/observable.py
@@ -1,12 +1,15 @@
 from typing import Protocol
 
 
+from .observer import ObserverInterface
+
+
 class ObservableInterface(Protocol):
 
-    def register_observer(self, observer):
+    def register_observer(self, observer: ObserverInterface):
         ...
 
-    def remove_observer(self, observer):
+    def remove_observer(self, observer: ObserverInterface):
         ...
 
     def notify_observers(self):
---
+++ 02_observer/weatherstation.py
@@ -0,0 +1,48 @@
+from typing import List
+from dataclasses import dataclass
+
+from interfaces import ObservableInterface, ObserverInterface
+
+
+@dataclass()
+class WeatherInfo:
+    temperature: float
+    pressure: float
+    humidity: float
+
+
+class WeatherStation(ObservableInterface):
+    __temperature: float = 0.0
+    __humidity: float = 0.0
+    __pressure: float = 760
+    __observers: List[ObserverInterface]
+
+    def register_observer(self, observer: ObserverInterface):
+        self.__observers.append(observer)
+
+    def remove_observer(self, observer: ObserverInterface):
+        self.__observers.remove(observer)
+
+    def notify_observers(self):
+        for observer in self.__observers:
+            observer.update()
+
+    def get_measurements(self) -> WeatherInfo:
+        return WeatherInfo(temperature=self.temperature, humidity=self.humidity, pressure=self.pressure)
+
+    def set_measurements(self, temperature: float, humidity: float, pressure: float):
+        self.__temperature = temperature
+        self.__humidity = humidity
+        self.__pressure = pressure
+
+    @property
+    def temperature(self) -> float:
+        return self.__temperature
+
+    @property
+    def pressure(self) -> float:
+        return self.__pressure
+
+    @property
+    def humidity(self) -> float:
+        return self.__humidity
