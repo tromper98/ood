c0efd63126f839136579da10907f13c591eefe60
tromper98 <koloff.daniil@yandex.ru>
2023-03-12 13:00:05 +0300

    lab-03 1. Добавил Milkshake 2. Добавил порции напиткам

--- decorator_03/impl/beverage.py
+++ decorator_03/impl/beverage.py
@@ -1,3 +1,5 @@
+from enum import Enum
+
 from .interfaces import BeverageInterface
 
 
@@ -11,33 +13,80 @@ class Beverage(BeverageInterface):
         return self._description
 
 
+class CoffeePortion(Enum):
+    SINGLE = 'single'
+    DOUBLE = 'double'
+
+
 class Coffee(Beverage):
-    def __init__(self, description: str = 'Coffee'):
+    _portion: CoffeePortion
+
+    def __init__(self, description: str = 'Coffee', portion: CoffeePortion = CoffeePortion.SINGLE):
         super().__init__(description)
+        self._portion = portion
 
     def get_cost(self) -> float:
         return 60
 
 
 class Cappuccino(Coffee):
-    def __init__(self):
-        super().__init__('Cappuccino')
+
+    def __init__(self, portion: CoffeePortion):
+        super().__init__(f'{portion.value} Cappuccino', portion)
 
     def get_cost(self) -> float:
-        return 80
+        cost = 120 if self._portion.DOUBLE else 80
+        return cost
 
 
 class Latte(Coffee):
-    def __init__(self):
-        super().__init__('Latte')
+    _portion: CoffeePortion
+
+    def __init__(self, portion: CoffeePortion):
+        super().__init__(f'{portion.value} Latte', portion)
 
     def get_cost(self) -> float:
-        return 90
+        cost = 130 if self._portion.DOUBLE else 90
+        return cost
+
+
+class TeaType(Enum):
+    OOLONG = 'Oolong'
+    BLACK = 'Black'
+    GREEN = 'Green'
+    HERBAL = 'Herbal'
 
 
 class Tea(Beverage):
-    def __init__(self):
-        super().__init__('Tea')
+    _tea_type: TeaType
+
+    def __init__(self, tea_type: TeaType):
+        super().__init__(f'{tea_type.value} Tea')
+        self._tea_type = tea_type
 
     def get_cost(self) -> float:
         return 30
+
+
+class MilkshakePortion(Enum):
+    SMALL = 'Small'
+    MEDIUM = 'Medium'
+    BIG = 'Big'
+
+
+class Milkshake(Beverage):
+    _portion: MilkshakePortion
+
+    def __init__(self, portion: MilkshakePortion):
+        super().__init__(f'{portion.value} Milkshake')
+        self._portion = portion
+
+    def get_cost(self) -> float:
+        if self._portion.SMALL:
+            cost = 50
+        elif self._portion.MEDIUM:
+            cost = 60
+        else:
+            cost = 80
+
+        return cost
